322aa7443ba92f9daf7b3b3ba99d7328e4f189d2
Vu Hoang Phuc <vuhoangphuc272@gmail.com>
2022-01-19 19:08:08 +0700

    Set admin account

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import { adminAccount } from "./constants/error-message-signIn";
+import { useEffect } from "react";
+import { setItemWithLocal } from "./utils/process-data";
 
 function App() {
-  return <div className="App">
-    {/* <SignIn/> */}
-    <SignUp/>
+  function setAdminAccount() {
+    if (JSON.parse(localStorage.getItem("userdata"))) {
+      return;
+    }
+    setItemWithLocal("userdata", [adminAccount]);
+    return;
+  }
+  useEffect(() => {
+    setAdminAccount();
+  }, []);
 
-  </div>;
+  return (
+    <div className="App">
+      <SignIn />
+      {/* <SignUp /> */}
+    </div>
+  );
 }
 
 export default App;
