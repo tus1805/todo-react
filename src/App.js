67023c715755a6c5a792e3923eb4aa2e84d716be
Tus Nguyen <tusnguyen1805@gmail.com>
2022-02-09 20:33:55 +0700

    ko biet sao xot 2 file a...

--- src/App.js
+++ src/App.js
@@ -1,62 +1,75 @@
-import "./App.css";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import ToDoList from "./pages/ToDoList"
-import { adminAccount } from "./constants/error-message-signIn";
-import { useEffect, useState } from "react";
-import { setItemWithLocal } from "./utils/process-data";
-import ProjectAdmin from "./pages/Project-admin";
-import UserAdmin from "./pages/User-admin";
+// import "./App.css";
+// import SignIn from "./pages/SignIn";
+// import SignUp from "./pages/SignUp";
+// import ToDoList from "./pages/ToDoList"
+// import { adminAccount } from "./constants/error-message-signIn";
+// import { useEffect, useState } from "react";
+// import { setItemWithLocal } from "./utils/process-data";
+// import ProjectAdmin from "./pages/Project-admin";
+// import UserAdmin from "./pages/User-admin";
 
-function App() {
-  const [page, setPage] = useState(0);
+// function App() {
+//   const [page, setPage] = useState(0);
 
-  function setAdminAccount() {
-    if (JSON.parse(localStorage.getItem("userdata"))) {
-      return;
-    }
-    setItemWithLocal("userdata", [adminAccount]);
-    return;
-  }
-  useEffect(() => {
-    setAdminAccount();
-  }, []);
+//   function setAdminAccount() {
+//     if (JSON.parse(localStorage.getItem("userdata"))) {
+//       return;
+//     }
+//     setItemWithLocal("userdata", [adminAccount]);
+//     return;
+//   }
+//   useEffect(() => {
+//     setAdminAccount();
+//   }, []);
 
-  useEffect(() => {
-    switchPage();
-  }, [page]);
+//   useEffect(() => {
+//     switchPage();
+//   }, [page]);
 
-  function changeLink(pageNum) {
-    setPage(pageNum);
-  }
+//   function changeLink(pageNum) {
+//     setPage(pageNum);
+//   }
 
-  console.log("chuyển page", page);
+//   console.log("chuyển page", page);
 
-  function switchPage() {
-    switch (page) {
-      case 0:
-        return <SignIn changeLink={changeLink} />;
-      case 1:
-        return <SignUp changeLink={changeLink} />;
-      case 2:
-        return <UserAdmin changeLink={changeLink} />;
-      case 3:
-        return <ToDoList changeLink={changeLink} />;
-      case 4:
-        return <ProjectAdmin changeLink={changeLink} />;
-      default:
-        return <ToDoList changeLink={changeLink} />;
-    }
-  }
+//   function switchPage() {
+//     switch (page) {
+//       case 0:
+//         return <SignIn changeLink={changeLink} />;
+//       case 1:
+//         return <SignUp changeLink={changeLink} />;
+//       case 2:
+//         return <UserAdmin changeLink={changeLink} />;
+//       case 3:
+//         return <ToDoList changeLink={changeLink} />;
+//       case 4:
+//         return <ProjectAdmin changeLink={changeLink} />;
+//       default:
+//         return <ToDoList changeLink={changeLink} />;
+//     }
+//   }
 
+//   return (
+//     <div className="App">
+//       {/* <SignIn /> */}
+//       {/* <SignUp /> */}
+//       {/* {switchPage()} */}
+//       {/* <ToDoList /> */}
+//       {/* <ProjectAdmin/> */}
+//       <UserAdmin/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Image from "./Image";
+
+function App(props) {
   return (
-    <div className="App">
-      {/* <SignIn /> */}
-      {/* <SignUp /> */}
-      {/* {switchPage()} */}
-      {/* <ToDoList /> */}
-      {/* <ProjectAdmin/> */}
-      <UserAdmin/>
+    <div className="app">
+      <Image/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from "react-dom";
+// import App from "./App";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeContextProvider } from "./themeContext";
+
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeContextProvider>
     <App />
-  </React.StrictMode>,
+  </ThemeContextProvider>,
   document.getElementById("root")
 );
