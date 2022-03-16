72660c7a237d525f827efee7df50db76098bf9da
Vu Hoang Phuc <vuhoangphuc272@gmail.com>
2022-03-16 20:38:54 +0700

    =(

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
+describe("App component", () => {
+  //Test 1: Ví dụ test quản lý state cơ bản
+  it("starts with a count of 0", () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find("div.counter-value").text();
+    expect(text).toEqual("Count: 0");
+  });
+  //Test 2: Ví dụ test tương tác của người dùng
+  it("increments count by 1 when the increment button is clicked", () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find("button.increment");
+    incrementBtn.simulate("click");
+    const text = wrapper.find("div.counter-value").text();
+    expect(text).toEqual("Count: 1");
+  });
+});
